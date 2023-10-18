ba98097b1813207efc83a579191e7617df16b0e8
tito-pereira <tab.pereira00@gmail.com>
2023-10-18 22:02:14 +0200

    m

---
+++ extra/main_bonus.c
@@ -0,0 +1,95 @@
+#include "fdf.h"
+
+int	escape_close(int keycode, void *param)
+{
+	t_all *all;
+	
+	all = (t_all *)param;
+	if (keycode == 65307)
+	{
+		mlx_destroy_image(all->mlx->mlx, all->mlx->img);
+		mlx_destroy_window(all->mlx->mlx, all->mlx->win);
+		mlx_loop_end(all->mlx->mlx);
+	}
+	return (0);
+}
+
+int mouse_handler(int button, int x, int y, void *param)
+{
+	t_all	*all;
+
+	all = (t_all *)param;
+	if (x > 0 && y > 0)
+	{
+		if (button == 4)
+			change_frame(all, 2, 1);
+		else if (button == 5)
+			change_frame(all, 2, 2);
+	}
+	return 0;
+}
+
+int	key_handler(int keycode, void *param)
+{
+	if (keycode == 119)
+		change_frame((t_all *)param, 1, 1);
+	else if (keycode == 97)
+		change_frame((t_all *)param, 1, 2);
+	else if (keycode == 115)
+		change_frame((t_all *)param, 1, 3);
+	else if (keycode == 100)
+		change_frame((t_all *)param, 1, 4);
+	else if (keycode == 32)
+		change_frame((t_all *)param, 3, 1);
+	else if (keycode == 49)
+		change_frame((t_all *)param, 4, 1);
+	else if (keycode == 65363)
+		change_frame((t_all *)param, 5, 1);
+	else if (keycode == 65361)
+		change_frame((t_all *)param, 5, 2);
+	else if (keycode == 50)
+		change_frame((t_all *)param, 6, 1);
+	else if (keycode == 65307)
+		escape_close(keycode, param);
+	return 0;
+}
+
+/*else if (keycode == 65362)
+	change_frame((t_all *)param, 2, 1);
+else if (keycode == 65364)
+	change_frame((t_all *)param, 2, 2);*/
+
+t_all	*create_all(t_mlx *mlx, t_grid *grid, t_point *pts)
+{
+	t_all	*all;
+
+	all = malloc(sizeof(t_all));
+	all->mlx = mlx;
+	all->grid = grid;
+	all->pts = pts;
+	return (all);
+}
+
+int	main(int ac, char **av)
+{
+	t_mlx	*mlx;
+	t_grid	*grid;
+	t_point	*pts;
+	t_all	*all;
+
+	if (ac == 2 && check_file(av[1]) == 1)
+	{
+		grid = create_grid(av[1]);
+		pts = create_points(grid);
+		mlx = malloc(sizeof(t_mlx));
+		mlx->mlx = mlx_init();
+		mlx->win = mlx_new_window(mlx->mlx, WIDTH, HEIGHT, "fdf");
+		all = create_all(mlx, grid, pts);
+		new_iso(all);
+		mlx_key_hook(mlx->win, key_handler, (void *)all);
+		mlx_mouse_hook(mlx->win, mouse_handler, (void *)all);
+		mlx_loop(mlx->mlx);
+		//free_everything(all);
+		mlx_destroy_display(mlx->mlx);
+	}
+}
---
+++ extra/planar_bonus.c
@@ -0,0 +1,51 @@
+#include "fdf.h"
+
+t_matrix	*plan_matrix(t_grid *grid)
+{
+	t_matrix	*pm;
+
+	pm = malloc(sizeof(t_matrix));
+	pm->y = 0;
+	if (grid->rows > 200)
+		pm->x = 2;
+	else if (grid->rows > 100 && grid->rows <= 200)
+		pm->x = 4;// * SCALE;
+	else if (grid->rows > 50 && grid->rows <= 100)
+		pm->x = 8;// * SCALE;
+	else if (grid->rows > 25 && grid->rows <= 50)
+		pm->x = 16;// * SCALE;
+	else if (grid->rows <= 25)
+		pm->x = 32;// * SCALE;
+	return (pm);
+}
+
+void	planar(t_all *all, int order)
+{
+	t_matrix	*row;
+	t_matrix	*line;
+	t_matrix	*start;
+
+	row = plan_matrix(all->grid);
+	line = malloc(sizeof(t_matrix));
+	line->x = 0;
+	line->y = 0;
+	start = malloc(sizeof(t_matrix));
+	start->x = (WIDTH / 12);
+	if (all->grid->rows > 200)
+		start->x = 50;
+	start->y = (HEIGHT / 8) * 7;
+	if (order == 1)
+		prep_pts(all, row, line, start, 5);
+	free(row);
+	free(line);
+	free(start);
+}
+
+/*
+devia arranjar uma max height para usar aqui na posição inicial
+
+(-1 * nmb_module((rm->x + lm->x) / 2) * iter->z)
+
+max_height = iter->z
+(rm->x / 2) * max_height e fazer contas por aí
+*/
