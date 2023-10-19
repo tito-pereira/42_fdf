d0776f8b2b5950d129de96a49f0188ffda4f5a05
tito-pereira <tab.pereira00@gmail.com>
2023-10-20 00:25:03 +0200

    m

--- extra/incline_bonus.c
+++ extra/incline_bonus.c
@@ -12,8 +12,6 @@ int	inc_down_iso(t_matrix *rm, t_matrix *lm)
 		//printf("after, ry:%d ly:%d\n", rm->y, lm->y);
 		if (rm->y != 0 && nmb_module(rm->x) / nmb_module(rm->y) == 1)
 			return (0);
-		/*else if (rm->y < 0 && nmb_module(rm->x) / nmb_module(rm->y) == 2)
-			return (3);*/
 		else if (rm->y == 0)
 			return (33);
 		return (3);
@@ -27,8 +25,6 @@ int	inc_down_iso(t_matrix *rm, t_matrix *lm)
 		//printf("after, ry:%d ly:%d\n", rm->y, lm->y);
 		if (rm->y != 0 && nmb_module(rm->x) / nmb_module(rm->y) == 1) //os dois extremos
 			return (0);
-		//else if (rm->y < 0 && nmb_module(rm->x) / nmb_module(rm->y) == 2)
-			//return (3);
 		else if (rm->y == 0)
 			return (33);
 		return (3);
@@ -36,15 +32,38 @@ int	inc_down_iso(t_matrix *rm, t_matrix *lm)
 	return (-1);
 }
 
-/*
 int	inc_down_plan(t_matrix *rm, t_matrix *lm)
-{}
-*/
+{
+	if ((rm->y == 0 && lm->x == 0) //C
+		&& (!(lm->y != 0 && rm->x / lm->y == -1)))
+	{
+		//printf("hori\nbefore, rx:%d ly:%d\n", rm->x, lm->y);
+		lm->y -= (rm->x / 2);
+		//printf("after, rx:%d ly:%d\n", rm->x, lm->y);
+		if (lm->y != 0 && nmb_module(rm->x) / nmb_module(lm->y) == 1)
+			return (0);
+		else if (lm->y == 0)
+			return (55);
+		return (5);
+	}
+	else if ((rm->x == 0 && lm->y == 0)
+		&& (!(rm->y != 0 && lm->x / rm->y == -1))) //D
+	{
+		//printf("verti\nbefore, ry:%d lx:%d\n", rm->y, lm->x);
+		rm->y -= (lm->x / 2);
+		//printf("after, ry:%d lx:%d\n", rm->y, lm->x);
+		if (rm->y != 0 && nmb_module(lm->x) / nmb_module(rm->y) == 1)
+			return (0);
+		else if (rm->y == 0)
+			return (55);
+		return (5);
+	}
+	return (inc_down_iso(rm, lm));
+}
 
 int	inc_up_iso(t_matrix *rm, t_matrix *lm)
 {
-	if ((lm->x == -rm->x) //B
-		&& (!(rm->y != 0 && rm->x / rm->y == -1)))
+	if ((lm->x == -rm->x) && (!(rm->y != 0 && rm->x / rm->y == -1))) //B
 	{
 		//printf("before, ry:%d ly:%d\n", rm->y, lm->y);
 		rm->y -= (rm->x / 2); //1 ,-->+ (-)
@@ -52,14 +71,11 @@ int	inc_up_iso(t_matrix *rm, t_matrix *lm)
 		//printf("after, ry:%d ly:%d\n", rm->y, lm->y);
 		if (rm->y != 0 && nmb_module(rm->x) / nmb_module(rm->y) == 1)
 			return (0);
-		/*else if (rm->y < 0 && nmb_module(rm->x) / nmb_module(rm->y) == 2)
-			return (3);*/
 		else if (rm->y == 0)
 			return (33);
 		return (3);
 	}
-	else if ((lm->x == rm->x) //A
-		&& (!(rm->y != 0 && rm->x / rm->y == 1)))
+	else if ((lm->x == rm->x) && (!(rm->y != 0 && rm->x / rm->y == 1))) //A
 	{
 		//printf("before, ry:%d ly:%d\n", rm->y, lm->y);
 		rm->y += (rm->x / 2); //-1 ,+->- (-)
@@ -67,8 +83,6 @@ int	inc_up_iso(t_matrix *rm, t_matrix *lm)
 		//printf("after, ry:%d ly:%d\n", rm->y, lm->y);
 		if (rm->y != 0 && nmb_module(rm->x) / nmb_module(rm->y) == 1) //os dois extremos
 			return (0);
-		//else if (rm->y < 0 && nmb_module(rm->x) / nmb_module(rm->y) == 2)
-			//return (3);
 		else if (rm->y == 0)
 			return (33);
 		return (3);
@@ -76,54 +90,33 @@ int	inc_up_iso(t_matrix *rm, t_matrix *lm)
 	return (-1);
 }
 
-/*
-1 - rotacao infinita e inversao do pts->z
-
-2 - rotacao em nao iso e nao plan
-nao existe posicoes fora do scope do que eu ja planeei, por isso
-b funciona bem, a nao me parece
-
-mal resolva a inclinacao direito, a unica coisa q preciso de fazer nos extremos
-e inverter z, depois encaixa na inclinacao infinita
-*/
-
 int	inc_up_plan(t_matrix *rm, t_matrix *lm)
 {
 	if ((rm->y == 0 && lm->x == 0) //C
-		&& (!(lm->y < 0 && nmb_module(rm->x) / nmb_module(lm->y) == 1)))
+		&& (!(lm->y != 0 && rm->x / lm->y == -1)))
 	{
-		lm->y -= (nmb_module(rm->x) / 2);
-		if (lm->y < 0 && nmb_module(rm->x) / nmb_module(lm->y) == 1)
+		//printf("hori\nbefore, rx:%d ly:%d\n", rm->x, lm->y);
+		lm->y -= (rm->x / 2);
+		//printf("after, rx:%d ly:%d\n", rm->x, lm->y);
+		if (lm->y != 0 && nmb_module(rm->x) / nmb_module(lm->y) == 1)
 			return (0);
-		else if (lm->y < 0 && nmb_module(rm->x) / nmb_module(lm->y) == 2)
-			return (5);
 		else if (lm->y == 0)
 			return (55);
 		return (5);
 	}
-	else if ((rm->x == 0 && lm->y == 0) //D
-		&& (!(rm->y < 0 && nmb_module(lm->x) / nmb_module(rm->y) == 1)))
+	else if ((rm->x == 0 && lm->y == 0)
+		&& (!(rm->y != 0 && lm->x / rm->y == -1))) //D
 	{
-		rm->y -= (nmb_module(lm->x) / 2);
-		if (rm->y < 0 && nmb_module(lm->x) / nmb_module(rm->y) == 1)
+		//printf("verti\nbefore, ry:%d lx:%d\n", rm->y, lm->x);
+		rm->y -= (lm->x / 2);
+		//printf("after, ry:%d lx:%d\n", rm->y, lm->x);
+		if (rm->y != 0 && nmb_module(lm->x) / nmb_module(rm->y) == 1)
 			return (0);
-		else if (rm->y < 0 && nmb_module(lm->x) / nmb_module(rm->y) == 2)
-			return (5);
 		else if (rm->y == 0)
 			return (55);
 		return (5);
 	}
 	return (inc_up_iso(rm, lm));
-}
-
-int	inc_up_central(t_matrix *rm, t_matrix *lm)
-{
-	//pra ja nao inverter nada, apenas fazer travao
-	if (rm->y != 0 && rm->x / rm->y == -1) //B
-		return (0);
-	else if (rm->y != 0 && rm->x / rm->y == 1) //A
-		return (0);
-	return (inc_up_plan(rm, lm));
 }
 
 void    incline(t_all *all, int order)
@@ -136,16 +129,13 @@ void    incline(t_all *all, int order)
     rm = r_matrix(all->pts);
 	z = 0;
 	start = malloc(sizeof(t_matrix));
-	start->x = all->pts->pixx;
-	start->y = all->pts->pixy;
 	lm = l_matrix(all->pts, all->grid);
-	//printf("before\nrm:%d, %d\nlm:%d, %d\n", rm->x, rm->y, lm->x, lm->y);
-	//condicoes de inclinação maxima
 	if (order == 1)
 		z = inc_up_plan(rm, lm);
-	//printf("after\nrm:%d, %d\nlm:%d, %d\n", rm->x, rm->y, lm->x, lm->y);
 	else if (order == 2)
-		z = inc_down_iso(rm, lm);
+		z = inc_down_plan(rm, lm);
+	start->x = (WIDTH / 2) - ((all->grid->rows / 2) * rm->x) - ((all->grid->lines / 2) * lm->x);
+	start->y = (HEIGHT / 2) - ((all->grid->rows / 2) * rm->y) - ((all->grid->lines / 2) * lm->y);
     if (z != -1)
 		prep_pts(all, rm, lm, start, z);
 	free(rm);
